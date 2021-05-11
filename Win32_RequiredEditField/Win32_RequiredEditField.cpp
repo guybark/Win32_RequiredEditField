afb93e8bcf85543e46e8ce95119b1bc3f76f4d4f
TheDoctor <TheDoctor@Gallifrey.org>
2021-05-11 15:27:05 -0700

    Add project files.

---
+++ Win32_RequiredEditField/Win32_RequiredEditField.cpp
@@ -0,0 +1,250 @@
+// Win32_RequiredEditField.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "Win32_RequiredEditField.h"
+
+// Barker: Near the top of the C++ file.
+#include <initguid.h>
+#include "objbase.h"
+#include "uiautomation.h"
+IAccPropServices* _pAccPropServices = NULL;
+// End of Barker edit.
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WIN32REQUIREDEDITFIELD, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32REQUIREDEDITFIELD));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32REQUIREDEDITFIELD));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WIN32REQUIREDEDITFIELD);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Barker: Add the following. 
+// Run when the UI is created.
+void SetEditFieldRequiredState(HWND hDlg, BOOL newRequiredState)
+{
+    HRESULT hr = S_OK;
+
+    if (_pAccPropServices == NULL)
+    {
+        hr = CoCreateInstance(
+            CLSID_AccPropServices,
+            nullptr,
+            CLSCTX_INPROC,
+            IID_PPV_ARGS(&_pAccPropServices));
+    }
+
+    if (SUCCEEDED(hr))
+    {
+        VARIANT var;
+        var.vt = VT_BOOL;
+        var.boolVal = VARIANT_TRUE;
+
+        // Mark the edit field as being required.
+        hr = _pAccPropServices->SetHwndProp(
+            GetDlgItem(hDlg, IDC_EDIT_NAME),
+            OBJID_CLIENT,
+            CHILDID_SELF,
+            IsRequiredForForm_Property_GUID,
+            var);
+    }
+}
+
+// Run when the UI is destroyed.
+void ClearEditFieldRequiredState(HWND hDlg)
+{
+    if (_pAccPropServices != nullptr)
+    {
+        // We only added the one property to the hwnd.
+        MSAAPROPID props[] = { IsRequiredForForm_Property_GUID };
+        HRESULT hr = _pAccPropServices->ClearHwndProps(
+            GetDlgItem(hDlg, IDC_EDIT_NAME),
+            OBJID_CLIENT,
+            CHILDID_SELF,
+            props,
+            ARRAYSIZE(props));
+
+        _pAccPropServices->Release();
+        _pAccPropServices = NULL;
+    }
+}
+// End of Barker edit.
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+    {
+        // Barker: Say the edit field is required.
+        SetEditFieldRequiredState(hDlg, TRUE);
+
+        return (INT_PTR)TRUE;
+    }
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+
+    case WM_DESTROY:
+
+        // Barker: Clear the custom property set on the edit field.
+        ClearEditFieldRequiredState(hDlg);
+
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
